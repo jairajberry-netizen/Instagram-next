9463bbfa7e73bdbde02d44c2651e604ac422909e
Jairaj Berry <jairajberry@gmail.com>
2026-05-16 15:36:16 +0530

    initialized message section

--- app/messages/NoteCard.tsx
+++ app/messages/NoteCard.tsx
@@ -8,7 +8,12 @@ const NoteCard = ({ CardImage, UserNote }: NoteCardProps) => {
     <div className="flex mt-2  ">
       <div className="bg-white h-16 w-16 ml-3 rounded-full flex items-center justify-center bg-gradient-to-tr from-yellow-400 via-pink-500 to-purple-600 ">
         <div className="rounded-full w-14 h-14   shrink-0 bg-zinc-900 relative border-2 border-black">
-          <Image src={CardImage} className="rounded-full object-cover" fill />
+          <Image
+            src={CardImage}
+            alt="image"
+            className="rounded-full object-cover"
+            fill
+          />
         </div>
         <span className="text-gray-300 text-xs">{UserNote}</span>
       </div>
